efa70a4ab7d5d19ea7ed7af073fcaa4ce535a17f
amirkacem <amir.kassem.ben.ammou@gmail.com>
2021-04-28 02:55:50 +0200

    Book Comment & home Show books

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,8 +17,19 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home.index")
      */
-    public function index(): Response
-    {
-        return $this->render('website/pages/home.html.twig');
+    public function index(PaginatorInterface $paginator,Request $request): Response
+    {   $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+        $nb_books = count($books);
+        $books = $paginator->paginate(
+           $books,
+            $request->query->getInt('page',1),
+            9
+        );
+        return $this->render('website/pages/home.html.twig',
+        [
+            'books'=>$books,
+            'nb_books'=>$nb_books
+        ]
+        );
     }
 }
